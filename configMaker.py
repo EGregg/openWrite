1c6829be1f98a533825e8cb24e27b2208c5e3eb0
eg <creosino@gmail.com>
2017-08-30 14:27:31 -0400

    config_read now asks for user info when not present

--- configMaker.py
+++ configMaker.py
@@ -4,25 +4,27 @@ import os
 
 credentialsFile = 'storedCredentials.txt'
 
+#creates credentials file if not present
 def config_present():
 	try:
 	        open(credentialsFile,'r')
 	except:
 		open(credentialsFile,'w')
 
+#asks for username and password to be written to credentials file
 def config_writer():
 	username = input('create username: ')
 	password = input('create password: ')
 	fd = open(credentialsFile,"w")
 	fd.write("%s:%s" % (username,password))
 
+#reads credentials file for user info, creates if not present
 def config_read():
 	with open(credentialsFile) as f:
+		if os.stat(credentialsFile).st_size == 0:
+			config_writer()
+		
 		credentials = [x.strip().split(':') for x in f.readlines()]
-		#if the file is empty then to run config_writer
-
-	for username,password in credentials:
-		print (username, password)
 
 config_present()
 config_read()
