dd41e5ff09e2bc0b227376bdfa5e1aed7843c908
eg <creosino@gmail.com>
2017-12-21 12:36:48 -0500

    updated comments and variable accession outside configRead function

--- configMaker.py
+++ configMaker.py
@@ -5,26 +5,41 @@ import os
 credentialsFile = 'storedCredentials.txt'
 
 #creates credentials file if not present
-def config_present():
+def configPresent():
 	try:
 	        open(credentialsFile,'r')
 	except:
 		open(credentialsFile,'w')
 
 #asks for username and password to be written to credentials file
-def config_writer():
+def configWriter():
 	username = input('create username: ')
 	password = input('create password: ')
 	fd = open(credentialsFile,"w")
 	fd.write("%s:%s" % (username,password))
 
 #reads credentials file for user info, creates if not present
-def config_read():
+#stores
+def configReader():
 	with open(credentialsFile) as f:
 		if os.stat(credentialsFile).st_size == 0:
-			config_writer()
+			configWriter()
 		
 		credentials = [x.strip().split(':') for x in f.readlines()]
+		for p in credentials:
+			username = p[0]
+			password = p[1]
 
-config_present()
-config_read()
+		return username,password
+		
+
+#first check to see if the file is present
+configPresent()
+
+#then read through the file and store the username as username and password as password
+configReader()
+
+#sets the username and password for use out of the function
+config_username, config_password = configReader()
+#print (config_username)
+#print (config_password)
